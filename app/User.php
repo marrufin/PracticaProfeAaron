616f11e8d79483f8216ca4cdf22e7155776fa8c2
marrufin <marrufojosecarlos@gmail.com>
2019-11-06 14:50:20 -0600

    [ADD] Librerías

--- app/User.php
+++ app/User.php
@@ -16,24 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nombre', 'email', 'password','apellido','sexo','fecha_nacimiento','celular'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Mail\MessageMail;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('vistas.home');
+    return redirect('home');
 });
+Route::get('/home', function () {
+    return view('vistas.home');
+})->name('home');
+
+Route::get('/auth', function(){
+    return view('vistas.auth');
+})->middleware('auth');
+
+Route::match(['get','post'], 'logout', function () {
+    Auth::logout();
+    return redirect('login');
+});
+
+Route::put('email', function(\Request $request){
+    \Mail::send(new MessageMail($request->email));
+    return response()->json(["message" => 'Hola']);
+});
+
+Route::match(['GET', 'POST', 'PUT'],'/login', 'UserController@main')->name('login');
 
 Route::get('/jose-carlos-marrufo', 'PracticaController@JoseCarlosMarrufoVazquez');
 Route::get('/VeronicaHuizar', 'PracticaController@VeronicaHuizar');
 Route::get('/Luis_Montes', 'PracticaController@LuisMontesMartinez');
 Route::get('/RicardoMartinez', 'PracticaController@RicardoMartinez');
 Route::get('/MiguelDuenez', 'PracticaController@MiguelDuenez');
-Route::get('/login', 'PracticaController@login');
 Route::get('/jerseys', 'PracticaController@jersey');
 Route::get('/ninos', 'PracticaController@ninos');
 Route::get('/jersey', 'PracticaController@jersey');
